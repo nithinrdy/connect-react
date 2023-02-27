bb7396452da974cb935a0d22720866b3a9102e92
Nithin <78612244+nithinrdy@users.noreply.github.com>
2023-02-27 18:13:29 +0530

    finish up dashboard page

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -1,21 +1,24 @@
 import useAuth from "../customHooksAndServices/authContextHook";
+import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import {
 	RouteTransitionVariants,
 	ConstituentPageElementsVariants,
 } from "../framerMotionVariants/generalVariants";
+import "../componentSpecificStyles/dashboardStyles.css";
 
 export default function DashboardComponent() {
 	const { user } = useAuth();
 
 	return (
 		<motion.div
+			className="w-full wide:h-screen flex flex-col justify-center"
 			variants={RouteTransitionVariants}
 			initial="initial"
 			animate="animate"
 			exit="exit"
 		>
-			<div className="flex flex-col items-center w-full text-white">
+			<div className="flex flex-row mob:flex-col mob:mt-32 items-center justify-evenly w-full h-40 text-white">
 				<motion.h1
 					variants={ConstituentPageElementsVariants}
 					className="text-7xl mt-14 mb-12 text-center"
@@ -23,6 +26,26 @@ export default function DashboardComponent() {
 				>
 					Hello {user.username}!
 				</motion.h1>
+				<div className="w-1 h-full mob:h-1 mob:w-4/5 border-2"></div>
+				<motion.div
+					variants={ConstituentPageElementsVariants}
+					className="flex flex-col text-2xl"
+					style={{ fontFamily: "Raleway" }}
+				>
+					<Link to="/connect" className="mt-8 flex flex-col mob:items-center">
+						<button className="border-b-2 dashboard-connect-button relative px-2 py-2 transition-colors hover:text-black mob:border-b-2 mob:bg-white mob:text-black">
+							Connect!
+						</button>
+					</Link>
+					<Link
+						to="/favorites"
+						className="mt-8  flex flex-col mob:items-center"
+					>
+						<button className="dashboard-favorites-button relative px-2 py-2 transition-colors hover:text-black mob:border-b-2">
+							View your favorites list
+						</button>
+					</Link>
+				</motion.div>
 			</div>
 		</motion.div>
 	);
